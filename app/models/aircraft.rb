63b1fcf52c619a621d675939806912beafbbe5e1
bweinmann <bsweinmann@gmail.com>
2022-02-24 19:41:42 -0500

    resetting db

--- app/models/aircraft.rb
+++ app/models/aircraft.rb
@@ -1,2 +1,11 @@
 class Aircraft < ApplicationRecord
+    has_many :reviews
+
+    def slugify
+        self.slug = name.parameterize
+    end
+
+    def average_score
+        reviews.average(:score).round(2).to_f
+    end
 end
