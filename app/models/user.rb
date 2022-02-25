9adc086156adefcea47c1dac9cd118e15f7db024
bweinmann <bsweinmann@gmail.com>
2022-02-25 15:44:27 -0500

    user model updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+    has_many :reviews
+    has_many :comments
     has_secure_password
+    
     validates :username, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
