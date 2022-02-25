8c85274d4a62cb604099e8893d6be0c9a901dd6e
bweinmann <bsweinmann@gmail.com>
2022-02-24 20:27:25 -0500

    auth request completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  post '/auth/login', to: 'authentication#login'
+  get '/auth/verify', to: 'authentication#verify'
+  
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
