a1ca31410e6f5c462fae6ac2d3d1b9d4e5c2ba8f
bweinmann <bsweinmann@gmail.com>
2022-02-26 21:27:38 -0500

    login/navigation initiated

--- client/src/components/Forms/Login.jsx
+++ client/src/components/Forms/Login.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
+// import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export default function Login() {
+
+
+
+export default function Login(props) {
+  const {input, setInput, setLogging } = props;
+
+  let navigate = useNavigate();
+ 
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLogging((prevlog)=>!prevlog);
+    navigate("/");
+  }
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    const {name, value}=e.target
+      setInput((prevInput) => ({
+      ...prevInput,
+      [name]: value
+    }))
+  }
+
   return (
-    <div>Login</div>
+    <form onSubmit={(e) => { handleSubmit(e) }}>
+      <label>Username</label>
+      <input name="username" placeholder="username" onChange={(e) => { handleChange(e) }} />
+      <label>Password</label>
+      <input name="password" placeholder="password" onChange={(e) => { handleChange(e) }} />
+      <button>Submit</button>
+    </form>
   )
 }
