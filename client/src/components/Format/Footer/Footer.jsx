cef195487f2d98795f771877e906337b8705d8fe
bweinmann <bsweinmann@gmail.com>
2022-02-27 14:58:27 -0500

    Footer mostly completed, will adjust format

--- client/src/components/Format/Footer/Footer.jsx
+++ client/src/components/Format/Footer/Footer.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import './Footer.css';
 
-export default function Footer() {
-  return (
-    <div>Footer</div>
-  )
+const Footer = () => {
+
+    return (  
+        <div className="main-footer">
+          <h4 className="footer-header">Contact Us</h4>
+            <div className="row">
+              <div className="col">
+                <ul className="list-unstyled">
+                 <a className="social-link" href="www.linkedin.com/in/blake-weinmann">LinkedIn</a>
+                </ul>
+              </div>
+            <div className="col">
+              <ul className="list-unstyled">
+               <a className="social-link" href="https://github.com/bweinmann">Github</a>
+              </ul>
+            </div>
+            <div className="col">
+             <ul className="list-unstyled">
+              <a className="social-link" href="/">Portfolio</a>
+              </ul>
+            </div>
+          </div>
+      </div>
+    )
 }
+
+export default Footer;
