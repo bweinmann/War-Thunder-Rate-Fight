c54b78b5be91f40f26669250bf4d7b3ba0234a9a
bweinmann <bsweinmann@gmail.com>
2022-02-27 14:07:53 -0500

    switched to rendering screens, home page completed, css started

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import "./HomePage.css"
+import Display from "../../components/Display/Display"
+import {Link} from "react-router-dom"
 
-export default function Home() {
+export default function HomePage(props) {
   return (
-    <div>Home</div>
+    <div>
+      <Display user={props.user}>
+        <div className="home-container">
+          <div className="home-container-left">
+            <Link to="/reviews" style={{textDecoration: "none"}}>
+              <button className="home-button-left">View Reviews</button>
+            </Link>
+          </div>
+          <div className="home-container-right">
+            <Link to="/signup" style={{textDecoration: "none"}}>
+              <button className="home-button-right">Sign Up</button>
+            </Link>
+          </div>
+        </div>
+      </Display>
+    </div>
   )
 }
