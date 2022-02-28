491f6b16d16bbb6333884628403cd5cbe4f702c9
bweinmann <bsweinmann@gmail.com>
2022-02-28 12:29:17 -0500

    user has many aircrafts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_many :reviews
     has_many :comments
+    has_many :aircrafts
     has_secure_password
     
     validates :username, presence: true, uniqueness: true
