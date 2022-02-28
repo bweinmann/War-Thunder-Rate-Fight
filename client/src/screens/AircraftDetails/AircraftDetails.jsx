d8e82516d700ff2e826445deca559aaa19b89a7f
bweinmann <bsweinmann@gmail.com>
2022-02-28 11:46:30 -0500

    details component destructured, need to move to screens

--- client/src/screens/AircraftDetails/AircraftDetails.jsx
+++ client/src/screens/AircraftDetails/AircraftDetails.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
+import axios from "axios"
+import "./AircraftDetails.css"
+// import Layout from "../../components/Format/Layout/Layout"
+import {useState, useEffect} from "react"
+import {useParams, useNavigate} from "react-router-dom"
 
-export default function AircraftDetails() {
+
+
+export default function CouchDetail(props) {
+
+  const [aircraft, setAircraft] = useState(null)
+  const {id} = useParams()
+  const user = localStorage.getItem("token")
+  const username = localStorage.getItem("username")
+  const navigate = useNavigate()
+  useEffect(() => {
+    const fetchCouch = async () => {
+      const res = await axios.get(URL, {
+        headers: {
+          Authorization: `${localStorage.getItem("token")}`,
+        },
+      })
+      setCouch(res.data)
+    }
+    fetchCouch()
+  }, [id])
+  
   return (
     <div>AircraftDetails</div>
   )
