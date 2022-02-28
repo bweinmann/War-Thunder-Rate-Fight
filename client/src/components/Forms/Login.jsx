c8dd70c6875b154b5a4bdaaf6b4d61a83f0c1ed3
bweinmann <bsweinmann@gmail.com>
2022-02-28 15:06:40 -0500

    login converted to screen, redacted states, data rendered from services

--- client/src/components/Forms/Login.jsx
+++ client/src/components/Forms/Login.jsx
@@ -1,79 +1,30 @@
-import React from 'react';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { loginUser } from '../../services/User'
-import Button from 'react-bootstrap/Button'
+import "./Login.css"
 
 export default function Login(props) {
-  const [input, setInput] = useState({
-    username: "",
-    password: "", 
-  }); 
-
-  let navigate = useNavigate();
-  const { setCurrentUser } = props;
- 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    let user = await loginUser(input);
-    setCurrentUser(user);
-    navigate("/");
-  };
-
-  const handleChange = (e) => {
-    e.preventDefault();
-    const {username, value}=e.target
-      setInput((prevInput) => ({
-      ...prevInput,
-      [username]: value
-    }))
-  }
-
   return (
-    <div>
-      <Button
-        className="signin-button"
-        onClick={(e) => {
-          toggleClass(e)
-        }}
-        variant="light"
-      >
-        Sign In
-      </Button>
-
-      <div className="form-container">
-
-        <form
-          onSubmit={(e) => {
-            handleSubmit(e)
+    <div className="login">
+      <form onSubmit={(e) => props.handleLogin(e)}>
+        <h3>Login</h3>
+        <input
+          type="text"
+          value={props.username}
+          onChange={(e) => {
+            props.setUsername(e.target.value)
           }}
-        >   
-          <fieldset className={field}>
-            <legend className="legend1">Sign In</legend>
-            <input
-              name="username"
-              value={user.username}
-              placeholder="username"
-              onChange={(e) => {
-                handleTextInput(e)
-              }}
-            ></input>
-            <br />
-            <input
-              name="password"
-              value={user.password}
-              placeholder="password"
-              onChange={(e) => {
-                handleTextInput(e)
-              }}
-            ></input>
-            <br />
-            <Button type="submit" variant="dark">
-              Sign In
-            </Button>
-          </fieldset>
-        </form>
-      </div>
+          placeholder="username"
+        />
+        <br />
+        <input
+          type="password"
+          value={props.password}
+          onChange={(e) => {
+            props.setPassword(e.target.value.toString())
+          }}
+          placeholder="create a password with at least 6 characters"
+        />
+        <br />
+        <button onSubmit={() => props.setToggleLogin((prevToggle) => !prevToggle)}>Submit</button>
+      </form>
     </div>
   )
 }
