d4c2ef0c808c7d082c50e5c544ff3c82ce8e9e27
bweinmann <bsweinmann@gmail.com>
2022-03-01 11:15:37 -0500

    aircraft list successfully rendering, need to render name and score

--- client/src/screens/AircraftList/AircraftList.jsx
+++ client/src/screens/AircraftList/AircraftList.jsx
@@ -1,24 +1,25 @@
 import "./AircraftList.css"
 import {useState, useEffect} from "react"
-import { Link } from 'react-router-dom'
-// import Aircraft from '../../components/Aircraft/Aircraft'
+import { Routes, Route } from 'react-router-dom'
+import Aircraft from '../../components/Aircraft/Aircraft'
 import { getAllAircraft } from '../../services/aircraft'
 import Layout from "../../components/Format/Layout/Layout"
 import Search from "../../components/Search/Search"
 import Sort from "../../components/Sort/Sort"
 import {asc, desc} from "../../utilities/Sort.js"
 
-export default function AircraftList() {
+export default function AircraftList(props) {
   const [aircrafts, setAircrafts] = useState([])
   const [searchParams, setSearchParams] = useState([])
   const [sort, setSort] = useState(false)
-  const [sortParams, setSortParams] = useState("title-ascending")
+  const [sortParams, setSortParams] = useState("name-ascending")
 
   useEffect(() => {
     const fetchAircraft = async () => {
       const resp = await getAllAircraft();
       setAircrafts(resp);
       console.log(resp);
+      setSearchParams(resp);
     }
     fetchAircraft()
   }, [])
@@ -27,9 +28,9 @@ export default function AircraftList() {
     if (type !== "" && type !== undefined) {
       setSortParams(type)
     }
-    if (type === "title-ascending") {
+    if (type === "name-ascending") {
       setSearchParams(asc(searchParams))
-    } else if (type === "title-descending") {
+    } else if (type === "name-descending") {
       setSearchParams(desc(searchParams))
 
     } else {
@@ -44,7 +45,7 @@ export default function AircraftList() {
 
   const handleSearch = (e) => {
     const searchResults = aircrafts.filter((aircraft) => 
-    aircraft.title.toLowerCase().includes(e.target.value.toLowerCase()))
+    aircraft.name.toLowerCase().includes(e.target.value.toLowerCase()))
     setSearchParams(searchResults)
     setSort(true)
   }
@@ -56,14 +57,14 @@ export default function AircraftList() {
       <Layout>
         <Search onSubmit={handleSubmit} handleSearch={handleSearch} />
           <Sort onSubmit={handleSubmit} handleSort={handleSort} />
-            {searchParams.map((aircraft) => {
-              <Link key={aircraft.id} to={`/aircraft/${aircraft.id}`}>
-              <img src={aircraft.image_url} />
-              <h4>{aircraft.title}</h4>
-              <h4>{aircraft.score}</h4>
-            </Link>
-            })}  
-            )
+          <Routes>
+            <Route path='/' element={
+          <Aircraft
+            aircrafts={aircrafts}
+            currentUser={props.currentUser}
+           />
+          } />
+        </Routes>
       </Layout>
     </div>
   )
