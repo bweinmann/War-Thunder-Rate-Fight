83a268a0ca32bab7b392e1b332a764ee074a4bec
bweinmann <bsweinmann@gmail.com>
2022-03-02 02:46:00 -0500

    navbar styled, need to implement hamburger button

--- client/src/components/Format/Navbar/Navbar.jsx
+++ client/src/components/Format/Navbar/Navbar.jsx
@@ -11,16 +11,18 @@ const Navbar = (props) => {
         <div className="nav">
            {currentUser ?
             <>
-                <Link to="/"><h2>War Thunder Rate Fights</h2></Link>
+                <Link className="nav-brand" to="/">War Thunder Rate Fights</Link>
                 <h3>Welcome, {currentUser.username}!</h3>
-                <Link to="/" onClick={logout}>Log Out</Link>
+                <Link className="nav-links" to="/" onClick={logout}>Log Out</Link>
             </>
                : 
                <> 
-              <Link to="/"><h2>War Thunder Rate Fights</h2></Link>
-              <Link to='/login'>Log In</Link>      
-              <Link to='/signup'>Sign Up</Link>
-              <Link to='/aircrafts'>View Aircaft</Link>
+                <Link className="nav-brand"to="/">War Thunder Rate Fights</Link>
+                <div className="nav-links-container">
+                  <Link className="nav-links"to='/login'>Log In</Link>      
+                  <Link className="nav-links"to='/signup'>Sign Up</Link>
+                  <Link className="nav-links"to='/aircrafts'>View Aircaft</Link>
+                </div>
               </>
               }
         </div>
